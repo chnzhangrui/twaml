be84bf1d7dfcbeb2beae5b19bc5f7503567c9dcd
Rui Zhang <rui.zhang@cern.ch>
2019-07-08 20:56:42 +0200

    Add inference

---
+++ advnn/apply.py
@@ -0,0 +1,79 @@
+from keras.models import model_from_json
+from argparse import ArgumentParser
+from root_numpy import tree2array, array2tree, array2root
+from shutil import copyfile
+from ROOT import TFile, TDirectory
+import numpy as np
+import pickle
+
+def loadModel(json_file, h5_file):
+    # load json and create model
+    json_file = open(json_file, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(h5_file)
+    return loaded_model
+
+def apply(json, h5, pkl, root_in, root_out):
+    inFile = TFile(root_in)
+    print(inFile.GetName())
+
+    copyfile(root_in, root_out)
+    print ('copy', root_in, root_out)
+    outBranch = 'DNN_response'
+
+    ''' implement ANN on each ttree '''
+    for name in ['AppInput']:
+        print('Implementing:', name)
+
+        ''' *array* is [eventVariables, EventWeight]; *event* is [eventVariables]; *weight* is [EventWeight]'''
+        tree_list = []
+        variables = []
+        weight = []
+        array = []
+        test_event = np.array
+
+        tree_list.append(inFile.Get(name))
+        variables.append(tree2array(tree_list[-1], branches=['Z_PT_FSR', 'Z_Y_FSR', 'Muons_CosThetaStar'], selection='1'))
+        weight.append(tree2array(tree_list[-1], branches=[ 'weight' ], selection='1'))
+        array.append([list(elem) for elem in zip(variables[-1], weight[-1])])
+
+        event__list = []
+        for ivar in variables:
+            for i in ivar:
+                event__list.append(list(i))
+        test_event = np.vstack(event__list)
+
+
+        #load the content
+        scaler = pickle.load(open(pkl, 'rb'))
+        test_event_transfered = scaler.transform(test_event)
+        predicttest__ANN = loadModel(json, h5).predict(test_event_transfered)
+
+
+        weight__list = []
+        for iwegt in weight:
+            for i in iwegt:
+                weight__list.append(list(i))
+        test_weight = np.vstack(weight__list)
+
+        response__test_event = np.column_stack((test_event, test_weight, predicttest__ANN))
+
+        variable_array = response__test_event[:,-1]
+        variable_array.dtype = [(outBranch, response__test_event.dtype)]
+        array2root(variable_array, treename=name, filename=root_out)
+
+
+    print(root_out, 'is saved and closed.')
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--input", dest="input", help="Input ROOT file", metavar="FILE", default='mc16a_345097_zero_jet.root')
+    parser.add_argument("--output", dest="output", help="Output ROOT file", metavar="FILE", default='out_mc16a_345097_zero_jet.root')
+    parser.add_argument("--json", dest="json", help="Model json file", metavar="FILE", default='../cernbox/twaml/job__l5n30_lr0.01mom0.8_elu_k3_e10/Train/0j_model.json')
+    parser.add_argument("--h5", dest="h5", help="Model weight h5 file", metavar="FILE", default='../cernbox/twaml/job__l5n30_lr0.01mom0.8_elu_k3_e10/Train/0j_model_0010.h5')
+    parser.add_argument("--pkl", dest="pkl", help="Event stored pkl file after transformation", metavar="FILE", default='../cernbox/twaml/job__l5n30_lr0.01mom0.8_elu_k3_e10/Train/0j_event.pkl')
+    args = parser.parse_args()
+    apply(root_in = args.input, root_out = args.output, json = args.json, h5 = args.h5, pkl = args.pkl)
